8341b2335355a881b2f2c273e61769295aa65b14
Rafferty Chen <raffertychen@gmail.com>
2024-03-10 05:03:21 -0700

    Switch IBS calculations to multiprocessing

--- relative_finding.py
+++ relative_finding.py
@@ -1,4 +1,7 @@
 import argparse
+import functools
+import pysam
+import subprocess
 import time
 import numpy as np
 import multiprocessing as mp
@@ -7,7 +10,26 @@ from cyvcf2 import VCF
 from pathlib import Path
 
 
+def get_stats(filename):
+    """
+    uses bcftools to get contig name, contig length, and number of records
+    will generate a .tbi file if one doesn't already exist
+    """
+    bcftools_output = subprocess.run(
+        ["bcftools", "index", "-t", "-s", filename], capture_output=True
+    )  # will generate tbi file if it doesn't already exist
+    ref, contig_length, num_snps = (
+        bcftools_output.stdout.decode("utf-8").strip().split("\t")
+    )
+
+    contig_length = int(contig_length)
+    num_snps = int(num_snps)
+
+    return ref, contig_length, num_snps
+
+
 def get_probs(filename, debug=False):
+    """calculates probabilities needed for IBD calculations"""
     records = VCF(filename, threads=mp.cpu_count() - 1)
     ps = np.zeros((9,))
 
@@ -25,7 +47,6 @@ def get_probs(filename, debug=False):
         p = record.aaf
         q = 1 - p
 
-        # TODO: add sample size correction terms
         ps[0] += 2 * p**2 * q**2
         ps[1] += 4 * p**3 * q + 4 * p * q**3
         ps[2] += p**4 + q**4 + 4 * p**2 * q**2
@@ -45,25 +66,29 @@ def get_probs(filename, debug=False):
     return ps
 
 
-def get_ibs(filename, sample1, sample2, debug=False):
-    records = VCF(filename, threads=mp.cpu_count() - 1)
-    records.set_samples([sample1, sample2])
+def get_sample_idx(filename):
+    """returns a mapping from sample names to indices"""
+    records = VCF(filename)
+    return {k: v for v, k in enumerate(records.samples)}
 
+
+def calc_ibs_segment(i1, i2, ref, start, end):
+    """calculates IBS for a segment of a VCF file"""
     ibs = np.zeros((3,), dtype=int)
 
-    start = time.time()
-
-    print("Calculating IBS at SNP: ")
-
-    for count, record in enumerate(records):
-        if count % 2_000 == 0:
-            print(f"{count}\t\t{time.time()-start:0.2f}s", end="\r", flush=True)
-
-        if len(record.ALT) > 1:
+    global tb
+    for record in tb.fetch(
+        reference=ref,
+        start=start,
+        end=end,
+        parser=pysam.asVCF(),
+        multiple_iterators=True,
+    ):
+        if "," in record.alt:  # skip snps with multiple alt alleles
             continue
 
-        sorted_gt_0 = sorted(record.genotypes[0][:2])
-        sorted_gt_1 = sorted(record.genotypes[1][:2])
+        sorted_gt_0 = sorted(record[i1].split("|"))
+        sorted_gt_1 = sorted(record[i2].split("|"))
 
         if sorted_gt_0[0] == sorted_gt_1[0] and sorted_gt_0[1] == sorted_gt_1[1]:
             index = 2
@@ -74,16 +99,30 @@ def get_ibs(filename, sample1, sample2, debug=False):
 
         ibs[index] += 1
 
-    print()
-
-    if debug:
-        print(ibs)
-        print(f"calculated in {time.time() - start:.2f} seconds")
-
     return ibs
 
 
+def get_ibs(ref, sample_idx, sample1, sample2, debug=False):
+    print("Calculating IBS")
+
+    i1 = sample_idx[sample1]
+    i2 = sample_idx[sample2]
+
+    start = time.time()
+
+    with mp.Pool() as p:
+        result = p.starmap(functools.partial(calc_ibs_segment, i1, i2, ref), regions)
+
+    if debug:
+        print(np.sum(result, axis=0))
+        print(f"calculated in {time.time() - start:.2f} seconds")
+
+    return np.sum(result, axis=0)
+
+
 def get_ibd(ps, ibs):
+    print("Calculating IBD")
+
     ibd0 = ibs[0] / ps[0]
     ibd1 = (ibs[1] - ps[1] * ibd0) / ps[4]
     ibd2 = (ibs[2] - ps[2] * ibd0 - ps[5] * ibd1) / ps[8]
@@ -103,6 +142,7 @@ def get_ibd(ps, ibs):
 
 
 def get_samples(filename):
+    """returns a list of sample names present in the specified VCF file"""
     records = VCF(filename)
     return records.samples
 
@@ -123,6 +163,15 @@ if __name__ == "__main__":
     vcf_file = Path(args.filename)
     assert vcf_file.exists(), "Please input a valid VCF file"
 
+    ref, contig_length, _ = get_stats(vcf_file)
+
+    cores = mp.cpu_count()
+    segment = contig_length // cores
+    regions = [[segment * i, segment * (i + 1) + 1] for i in range(cores)]
+    regions[-1][-1] = None
+
+    sample_idx = get_sample_idx(vcf_file)
+
     if args.sample1 is None and args.sample2 is None:
         with open("sample_list.txt", "w") as file:
             samples = get_samples(vcf_file)
@@ -135,10 +184,13 @@ if __name__ == "__main__":
             ps = get_probs(vcf_file, debug=False)
             np.save(cache, ps)
 
-        ibs = get_ibs(vcf_file, args.sample1, args.sample2, debug=True)
+    global tb  # sorry for use of global, but TabixFile is not picklable
+    tb = pysam.TabixFile(str(vcf_file))  # should always exist because of get_stats
 
-        p0, p1, p2 = get_ibd(ps, ibs)
+    ibs = get_ibs(ref, sample_idx, args.sample1, args.sample2, debug=True)
 
-        print(f"P(IBD=0) = {p0:.5f}")
-        print(f"P(IBD=1) = {p1:.5f}")
-        print(f"P(IBD=2) = {p2:.5f}")
+    p0, p1, p2 = get_ibd(ps, ibs)
+
+    print(f"P(IBD=0) = {p0:.5f}")
+    print(f"P(IBD=1) = {p1:.5f}")
+    print(f"P(IBD=2) = {p2:.5f}")
